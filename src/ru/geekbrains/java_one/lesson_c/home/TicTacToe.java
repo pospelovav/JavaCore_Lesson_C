6b88e827f7cd53dcc3fe34cc78ffb6ba3d53a49f
pospelovav <shnurokvtaige@yandex.ru>
2020-09-13 23:41:18 +0300

    Доработан метод checkWin (добавлены второстепенные выиграшные диагонали) Чуть улучшен ИИ (Ставит О рядом с последним Х случайным образом)

--- src/ru/geekbrains/java_one/lesson_c/home/TicTacToe.java
+++ src/ru/geekbrains/java_one/lesson_c/home/TicTacToe.java
@@ -13,6 +13,8 @@ public class TicTacToe {
     private static char[][] field;
     private static int fieldSizeX;
     private static int fieldSizeY;
+    private static int globX;
+    private static int globY;
 
     //init field
     private static void initField() {
@@ -55,6 +57,8 @@ public class TicTacToe {
             y = SCANNER.nextInt() - 1;
         } while (!isCellValid(x, y) || !isCellEmpty(x, y));
         field[y][x] = DOT_HUMAN;
+        globX = x;
+        globY = y;
     }
 
     private static boolean isCellEmpty(int x, int y) {
@@ -65,20 +69,50 @@ public class TicTacToe {
         return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
     }
 
+
     // aiTurn
     private static void aiTurn() {
-        int x;
-        int y;
+        /*
+        ИИ не удался(
+        Ставит О рядом с последним Х случайным образом
+         */
+        int x = globX;
+        int y = globY;
         do {
-            x = RANDOM.nextInt(fieldSizeX);
-            y = RANDOM.nextInt(fieldSizeY);
-        } while (!isCellEmpty(x, y));
+            x += RANDOM.nextInt(3) - 1;
+            y += RANDOM.nextInt(3) - 1;
+            if (x < 0){
+                x++;
+            } else if (x >= fieldSizeX){
+                x--;
+            }
+            if (y < 0){
+                y++;
+            } else if (y >= fieldSizeY){
+                y--;
+            }
+       } while (!isCellValid(x, y) || !isCellEmpty(x, y));
+
         field[y][x] = DOT_AI;
     }
 
     // checkWin
     private static boolean checkWin(char c) {
-        char[][] checkDiag = new char[2][fieldSizeX];   //массив для диагоналей
+        /*
+            Учитель, простите. Я опять много где нарушил принцип DRY (и не только)
+            В своё оправдание могу сказать только то, что сразу сделать нормально - мозгов не хватило, а
+            дошлифовать - времени не хватило
+         */
+        int colDiag;    //количество выигрышных диагоналей
+        int ElForWin;
+        if (fieldSizeX > 3){
+            ElForWin = fieldSizeX - 1;
+            colDiag = 6;
+        } else {
+            ElForWin = 3;
+            colDiag = 3;
+        }
+        char[][] checkDiag = new char[colDiag][fieldSizeX];   //массив для диагоналей
 
         for (int k = 0; k < 2; k++) {
             for (int l = 0; l < fieldSizeY; l++) {
@@ -88,7 +122,7 @@ public class TicTacToe {
                             fixCheck ++;
                         }
                         if ((i == fieldSizeX-1) || (field[l][i] != c)) {
-                            if (fixCheck < fieldSizeX-1) {  //количество элементов подряд (размерность - 1)
+                            if (fixCheck < ElForWin) {  //количество элементов подряд (размерность - 1)
                                 fixCheck = 0;
                             } else {return true;}
                         }
@@ -98,7 +132,7 @@ public class TicTacToe {
                             fixCheck ++;
                         }
                         if ((i == fieldSizeX-1) || (field[i][l] != c)) {
-                            if (fixCheck < fieldSizeX-1) {  //количество элементов подряд (размерность - 1)
+                            if (fixCheck < ElForWin) {  //количество элементов подряд (размерность - 1)
                                 fixCheck = 0;
                             } else {return true;}
                         }
@@ -107,17 +141,25 @@ public class TicTacToe {
                 }
             }
 
-            for (int i = 0; i < fieldSizeX; i++) {                       //диагонали
+            for (int i = 0; i < fieldSizeX; i++) {                       // главные диагонали
                 checkDiag[0][i] = field[i][i];
                 checkDiag[1][i] = field[i][fieldSizeX - i - 1];
+
             }
-            for (int g = 0; g < 2; g++) {
+            for (int i = 0; i < ElForWin; i++) {            //второстепенные выигрышные диагонали
+                checkDiag[2][i] = field[i][i+1];
+                checkDiag[3][i] = field[i+1][i];
+                checkDiag[4][i] = field[i][ElForWin - i - 1];
+                checkDiag[5][i] = field[i+1][ElForWin - i];
+            }
+
+            for (int g = 0; g < colDiag-1; g++) {
                 for (int j = 0, fixCheck = 0; j < fieldSizeX; j++) {
                     if (checkDiag[g][j] == c){
                         fixCheck ++;
                     }
                     if ((j == fieldSizeX-1) || (checkDiag[g][j] != c)) {
-                        if (fixCheck < fieldSizeX-1) {  //количество элементов подряд (размерность - 1)
+                        if (fixCheck < ElForWin) {  //количество элементов подряд (размерность - 1)
                             fixCheck = 0;
                         } else {return true;}
                     }
@@ -161,8 +203,8 @@ public class TicTacToe {
                 humanTurn();
                 printField();
                 if (gameChecks(DOT_HUMAN, "Human win")) break;
-                //aiTurn();
-                //printField();
+             //   aiTurn();
+              //  printField();
                 if (gameChecks(DOT_AI, "Computer win")) break;
             }
             System.out.println("To play again enter 'y'");
